6962dabcbc0b0e7768460d4a4bcc9078a529e576
RagezzNb <139761116+RagezzNb@users.noreply.github.com>
2024-03-02 14:31:34 +0300

    FILES ADDED

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-// Set the date we're counting down to (March 1, 2024)
-var countDownDate = new Date('March 25, 2024 00:00:00').getTime();
+// Set the date we're counting down to
+var countDownDate = new Date("Mar 31, 2024 00:00:00").getTime();
 
-// Update the countdown every 1 second
+// Update the countdown every second
 var x = setInterval(function() {
+
+    // Get the current date and time
     var now = new Date().getTime();
+
+    // Calculate the distance between now and the countdown date
     var distance = countDownDate - now;
-    
+
+    // Calculate days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
+    // Display the countdown
     document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
-    
+
+    // If the countdown is over, display a message
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("countdown").innerHTML = "EXPIRED";
+        document.getElementById("countdown").innerHTML = "Game is here!";
     }
 }, 1000);
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
